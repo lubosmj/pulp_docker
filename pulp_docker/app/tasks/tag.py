89d49b8ddb0a07df1bef9c0eb4133fb9fc4bdb30
Lubos Mjachky <lubos.mjachky@gmail.com>
2019-08-04 18:40:09 +0200

    Fix issue for tagging manifests with different digest

--- pulp_docker/app/tasks/tag.py
+++ pulp_docker/app/tasks/tag.py
@@ -3,8 +3,22 @@ from pulp_docker.app.models import ManifestTag, Manifest
 
 
 def create_new_repository_version(manifest_pk, tag, repository_pk):
+    """
+    Create a new repository version out of the passed tag name and the manifest.
+
+    If the tag name is already associated with an existing manifest with the same digest,
+    no new content is created. Note that a same tag name cannot be used for two different
+    manifests. Due to this fact, an old ManifestTag object is going to be removed from
+    a new repository version when a manifest contains a digest which is not equal to the
+    digest passed with POST request.
+    """
+
     manifest = Manifest.objects.get(pk=manifest_pk)
     artifact = manifest._artifacts.all()[0]
+
+    existing_manifest_tag_list = ManifestTag.objects.filter(name=tag).exclude(
+        tagged_manifest=manifest
+    )
 
     manifest_tag, _ = ManifestTag.objects.get_or_create(
         name=tag,
@@ -23,3 +37,4 @@ def create_new_repository_version(manifest_pk, tag, repository_pk):
 
     with RepositoryVersion.create(repository) as repository_version:
         repository_version.add_content(manifest_tag_list)
+        repository_version.remove_content(existing_manifest_tag_list)
